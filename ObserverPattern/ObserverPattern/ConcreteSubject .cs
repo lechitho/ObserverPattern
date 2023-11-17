27632fe83582bb0b372777d22b2ec5c013f2cacf
Tho Le <tholc@outlook.com>
2023-11-17 16:42:50 +0700

    Init Demo ObserverPattern

---
+++ ObserverPattern/ObserverPattern/ConcreteSubject .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ObserverPattern
+{
+    public class ConcreteSubject : Subject
+    {
+        private string subjectState;
+        // Gets or sets subject state
+        public string SubjectState
+        {
+            get { return subjectState; }
+            set { subjectState = value; }
+        }
+    }
+}
---
+++ ObserverPattern/ObserverPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteSubject s = new ConcreteSubject();
+            
+            s.Attach(new ConcreteObserver(s, "X"));
+            s.Attach(new ConcreteObserver(s, "Y"));
+            s.Attach(new ConcreteObserver(s, "Z"));
+
+            s.SubjectState = "ABC";
+            s.Notify();
+        }
+    }
+}
